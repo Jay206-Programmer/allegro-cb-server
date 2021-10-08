e2b08f3e9d9116a818ddf18bc153b775fdd0a0b5
Jay Shukla <jshukla@infosenseglobal.com>
2021-10-08 10:09:28 -0700

    Allowed origin

--- db_assistent_server/settings.py
+++ db_assistent_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-bwtqag)$((11*z#9svn&6drhlmmc0-c3w9%)qe4@(dzg-l92*$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com','https://allegro-cb.herokuapp.com/']
 
 
 # Application definition
